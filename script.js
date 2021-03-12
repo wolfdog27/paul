b3f33dae6b2adc422d8b14bf079fd4e3ae1690a0
wolfdog27 <kylenicholasanderson@gmail.com>
2021-03-12 12:08:31 -0800

    working contact form with validation

--- script.js
+++ script.js
@@ -1,43 +1,37 @@
 
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
 
-(function () {
-    'use strict';
-    window.addEventListener('load', function () {
-        // Get the forms we want to add validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
-})();
-
+// This is for EmailJS
 const btn = document.getElementById('button');
+const email = document.getElementById('reply_to')
+const emailE = document.getElementById('emailError')
 
 document.getElementById('form')
     .addEventListener('submit', function (event) {
         event.preventDefault();
+        emailE.textContent = ""
 
+        const validEmail = validateEmail(email.value)
+        if (!validEmail) {
+            emailE.textContent = "Please enter a valid e-mail."
+            return
+        }
         btn.value = 'Sending...';
 
         const serviceID = 'default_service';
         const templateID = 'contact_form';
-        if (event.target.classList.contains('was-validated')) {
-            emailjs.sendForm(serviceID, templateID, this)
-                .then(() => {
-                    btn.value = 'Send Email';
-                    $('#success_message').show();
-                    window.setTimeout(function () { location.reload() }, 3000)
-                }, (err) => {
-                    btn.value = 'Send Email';
-                    $('#error_message').show();
-                    window.setTimeout(function () { location.reload() }, 3000)
-                });
-        }
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Send Email';
+                $('#success_message').show();
+                window.setTimeout(function () { location.reload() }, 3000)
+            }, (err) => {
+                btn.value = 'Send Email';
+                $('#error_message').show();
+                window.setTimeout(function () { location.reload() }, 3000)
+            });
     });
+
